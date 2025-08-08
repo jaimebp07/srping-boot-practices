fc80359d35c34047b5ea374ae2c2aeb668a96f7a
jaimebp07 <andrescodigo07@gmail.com>
2025-08-07 22:14:25 -0500

    🎉 The observer pattern project begins

---
+++ stream-sub-obs/src/main/java/com/observercompany/stream_sub_obs/StreamSubObsApplication.java
@@ -0,0 +1,13 @@
+package com.observercompany.stream_sub_obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSubObsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamSubObsApplication.class, args);
+	}
+
+}
---
+++ stream-sub-obs/src/test/java/com/observercompany/stream_sub_obs/StreamSubObsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.observercompany.stream_sub_obs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamSubObsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
