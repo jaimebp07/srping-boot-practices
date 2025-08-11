9185ccbbb519320277a10c478e9c828b53ded1f8
jaimebp07 <andrescodigo07@gmail.com>
2025-08-10 20:57:35 -0500

    🎉 project reactor features project was started

---
+++ project-reactor-features/src/main/java/com/mycompany/project_reactor_features/ProjectReactorFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.project_reactor_features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReactorFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReactorFeaturesApplication.class, args);
+	}
+
+}
---
+++ project-reactor-features/src/test/java/com/mycompany/project_reactor_features/ProjectReactorFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.project_reactor_features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
