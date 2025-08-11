ba1aa1473cdb0d4f5fb5950af19d34f22d16a94c
jaimebp07 <andrescodigo07@gmail.com>
2025-08-11 17:56:35 -0500

    ⚗️  mono was created

--- project-reactor-features/src/main/java/com/mycompany/project_reactor_features/ProjectReactorFeaturesApplication.java
+++ project-reactor-features/src/main/java/com/mycompany/project_reactor_features/ProjectReactorFeaturesApplication.java
@@ -1,13 +1,23 @@
 package com.mycompany.project_reactor_features;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.java.Log;
+import reactor.core.publisher.Mono;
 
-@SpringBootApplication
+@Log
 public class ProjectReactorFeaturesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectReactorFeaturesApplication.class, args);
-	}
+		// Publisher
+		Mono<String> mono = Mono.just( "Hello World")
+			.doOnNext( value -> log.info("[onNext]: " + value))//Acción que se ejecuta cuando el valor pasa por el stream antes de llegar al suscriptor.
+			.doOnSuccess( value -> log.info( "[onSuccess]: " + value)) //Acción cuando el Mono termina con éxito
+			.doOnError( err -> log.info(err.getMessage()));
 
+		// Consumer
+		mono.subscribe(
+			data -> log.info( "Receiving data:" + data), //onNext → Qué hacer cuando llega un dato (data).
+			err -> log.info( "Error: " + err.getMessage()), //onError → Qué hacer si ocurre un error (err).
+			() -> log.info( "Complete success!") //onComplete → Qué hacer cuando el flujo termina con éxito.
+		);
+	}
 }
